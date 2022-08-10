61e374d2b387066d8c347dccd8331c50b069e092
calebnjw <lelebng@gmail.com>
2022-08-10 11:20:29 +0800

    removed unnecessary imports

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 
 const cors = require('cors');
-const methodOverride = require('method-override');
 
 require('dotenv').config();
 
@@ -29,18 +28,10 @@ app.use(cors({
   credentials: true,
   origin: FRONTEND_URL,
 }));
-// Set the Express view engine to expect EJS templates
-app.set('view engine', 'ejs');
 // Bind cookie parser middleware to parse cookies in requests
 app.use(cookieParser());
-// Bind Express middleware to parse request bodies for POST requests
-app.use(express.urlencoded({ extended: false }));
 // Bind Express middleware to parse JSON request bodies
 app.use(express.json());
-// Bind method override middleware to parse PUT and DELETE requests sent as POST requests
-app.use(methodOverride('_method'));
-// Expose the files stored in the public folder
-app.use(express.static('public'));
 
 // Bind route definitions to the Express application
 app.use('/cars', carRouter);
